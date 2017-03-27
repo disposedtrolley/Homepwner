a0743884f11d82a1f401882cb4a861ea6462aa19
James Liu <james_liu@fastmail.com>
2017-03-28 07:19:41 +1100

    added sections

--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -12,8 +12,28 @@ class ItemsViewController: UITableViewController {
     
     var itemStore: ItemStore!
     
+    struct Section {
+        var heading: String
+        var items: [Item]
+        
+        init(title: String, objects: [Item]) {
+            heading = title
+            items = objects
+        }
+    }
+    
+    var sections: [Section]!
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sections[section].heading
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemStore.allItems.count
+        return sections[section].items.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -23,10 +43,10 @@ class ItemsViewController: UITableViewController {
         // Set the text on the cell with the description of the item
         // that is at the nth index of items, where n = row this cell
         // will appear in on the tableview
-        let item = itemStore.allItems[indexPath.row]
+//        let item = itemStore.allItems[indexPath.row]
         
-        cell.textLabel?.text = item.name
-        cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+        cell.textLabel?.text = sections[indexPath.section].items[indexPath.row].name
+        cell.detailTextLabel?.text = "$\(sections[indexPath.section].items[indexPath.row].valueInDollars)"
         
         return cell
     }
@@ -40,5 +60,29 @@ class ItemsViewController: UITableViewController {
         let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
         tableView.contentInset = insets
         tableView.scrollIndicatorInsets = insets
+        
+        sections = getSectionsFromItemStore()
+    }
+    
+    func getSectionsFromItemStore() -> [Section] {
+        var sectionsArray = [Section]()
+        
+        var expensiveItemsArray = [Item]()
+        var cheapItemsArray = [Item]()
+        for item in itemStore.allItems {
+            if item.valueInDollars > 50 {
+                expensiveItemsArray.append(item)
+            } else {
+                cheapItemsArray.append(item)
+            }
+        }
+        
+        let expensiveItems = Section(title: "Expensive Items", objects: expensiveItemsArray)
+        let cheapItems = Section(title: "Cheap Items", objects: cheapItemsArray)
+        
+        sectionsArray.append(expensiveItems)
+        sectionsArray.append(cheapItems)
+        
+        return sectionsArray
     }
 }
